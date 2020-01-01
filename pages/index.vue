60738f80168b03fd4f46a64ae377cbcccb90743b
shidoitsuka <mail@standinshd.site>
2020-01-02 00:22:19 +0700

    another meta tags test

--- pages/index.vue
+++ pages/index.vue
@@ -15,9 +15,10 @@ export default {
       // opengraphs
       // prettier-ignore
       meta: [
-        { hid: 'og:title', property: 'og:title', content: 'standinshd' },
-        { hid: 'og:type', property: 'og:type', content: 'website' },
-        { hid: 'og:image', property: 'og:image', content: 'https://i.ibb.co/5LdywPX/6017da3ed8f203fe979b16dae1ad2259.png' }
+        { hid: 'description', name: 'og:description', property: 'og:description', content: 'Just another human that fell in love too much with </code>' },
+        { hid: 'title', name: 'og:title', property: 'og:title', content: 'standinshd' },
+        { hid: 'type', name: 'og:type', property: 'og:type', content: 'website' },
+        { hid: 'image', name: 'og:image', property: 'og:image', content: 'https://i.ibb.co/5LdywPX/6017da3ed8f203fe979b16dae1ad2259.png' }
       ]
     };
   }
