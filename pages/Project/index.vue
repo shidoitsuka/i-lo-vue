a947bb653ec998e36883897a4cf2f20fb66c6ebb
shidoitsuka <mail@standinshd.site>
2020-01-02 00:48:10 +0700

    meta tags again

--- pages/Project/index.vue
+++ pages/Project/index.vue
@@ -86,13 +86,13 @@ export default {
       // opengraphs
       // prettier-ignore
       meta: [
-        { hid: 'description', name: 'og:description', content: 'Project Showcase' },
-        { hid: 'og:description', name: 'og:description', content: 'Project Showcase' },
-        { hid: 'og:title', name: 'og:title', content: 'standinshd' },
-        { hid: 'apple-mobile-web-app-title', name: 'apple-mobile-web-app-title', content: 'standinshd' },
-        { hid: 'og:type', name: 'og:type', content: 'website' },
-        { hid: 'og:site_name', name: 'og:site_name', content: 'standinshd' },
-        { hid: 'og:image', name: 'og:image', content: 'https://i.ibb.co/HDtnrQ0/Miku-Chibi0.png' }
+        { hid: 'description', property: 'og:description', content: 'Project Showcase' },
+        { hid: 'og:description', property: 'og:description', content: 'Project Showcase' },
+        { hid: 'og:title', property: 'og:title', content: 'standinshd' },
+        { hid: 'apple-mobile-web-app-title', property: 'apple-mobile-web-app-title', content: 'standinshd' },
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+        { hid: 'og:site_property', property: 'og:site_property', content: 'standinshd' },
+        { hid: 'og:image', property: 'og:image', content: 'https://i.ibb.co/HDtnrQ0/Miku-Chibi0.png' }
       ]
     };
   }
