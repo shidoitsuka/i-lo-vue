b3fa0ce11687ec0d7f78ede95c092373c08ed9b6
shidoitsuka <mail@standinshd.site>
2020-01-02 00:02:58 +0700

    added meta tag for testing

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-<v-content>
-  <Main />
-</v-content>
+  <v-content>
+    <Main />
+  </v-content>
 </template>
 
 <script type="text/javascript">
@@ -9,6 +9,17 @@ import Main from "~/components/Main.vue";
 export default {
   components: {
     Main
+  },
+  head() {
+    return {
+      // opengraphs
+      // prettier-ignore
+      meta: [
+        { hid: 'og:title', property: 'og:title', content: 'standinshd' },
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+        { hid: 'og:image', property: 'og:image', content: 'https://i.ibb.co/5LdywPX/6017da3ed8f203fe979b16dae1ad2259.png' }
+      ]
+    };
   }
 };
 </script>
