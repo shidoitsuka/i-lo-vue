f910ba1ee7f0e1135290a43d617436ea8910951c
shidoitsuka <mail@standinshd.site>
2020-01-03 07:32:13 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
     title: "standinshd",
     meta: [
       { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" }
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       // prettier-ignore
       { hid: 'description', name: 'description', content: "Just another human that fell in love too much with </code>" }
     ],
