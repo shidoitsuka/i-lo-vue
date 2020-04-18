ebc00c7b6bc4e41449e0efc9157e52af1ecf7d6a
Brian <mail@standinshd.site>
2020-04-19 01:22:58 +0700

    updated projects data

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -4,27 +4,42 @@ export default [
     name: "Miku Bot",
     subtitle: "A just4fun discord bot.",
     description: "My first discord bot. I started this just for fun project when I was really active on discord.\nBack then, I don't really mind about its bug and stuffs, until I decided to take this project seriously because me & my friends actually had fun with it!",
-    language: "Javascript, Node.js",
-    frameworks: "Discord.js, SQLite, Enmap",
-    date: "2018 - present",
+    engines: ["javascript-plain", "nodejs-plain", "mysql-plain"],
     thumb: "miku.png"
   },
   {
     name: "Miku Site",
     subtitle: "A just4fun discord bot.",
-    description: "My first discord bot. I started this just for fun project when I was really active on discord.\nBack then, I don't really mind about its bug and stuffs, until I decided to take this project seriously because me & my friends actually had fun with it!",
-    language: "Javascript, Node.js",
-    frameworks: "Discord.js, SQLite, Enmap",
-    date: "2018 - present",
+    description: "Miku seems to be actively used by my friends (or maybe others too!) So I decided to create a website exclusively for miku!",
+    engines: ["html5-plain", "css3-plain", "javascript-plain", "jquery-plain", "nodejs-plain", "express-original"],
     thumb: "miku-site.png"
+  },
+  {
+    name: "Pi RFID",
+    subtitle: "Multi-purpose RFID scanner.",
+    description: "My highschool, SMK Krian 1 trusted me to make an RFID system for their new canteen payment method. This card will also contains student's identity which means the student ID itself has a built in RFID system. It is also has a multi-level door lock system as well.",
+    engines: ["html5-plain", "css3-plain", "javascript-plain", "nodejs-plain", "express-original", "mysql-plain", "cplusplus-plain", "python-plain"],
+    thumb: "rfid.png"
   },
   {
     name: "I Lo Vue",
     subtitle: "Yes, you read that right.",
-    description: "My first discord bot. I started this just for fun project when I was really active on discord.\nBack then, I don't really mind about its bug and stuffs, until I decided to take this project seriously because me & my friends actually had fun with it!",
-    language: "Javascript, Node.js",
-    frameworks: "Discord.js, SQLite, Enmap",
-    date: "2018 - present",
+    description: "This is what you're looking at, literally. I fell in love easily with stuffs and vue is one of it. So I decided to make my portofolio website!",
+    engines: ["vuejs-plain", "sass-original", "javascript-plain", "babel-plain", "nodejs-plain"],
     thumb: "i-lo-vue.png"
+  },
+  {
+    name: "SMK Jatim",
+    subtitle: "East Java's Vocational Highschool in one place.",
+    description: "At late Q1 of 2020 East Java's Government trusted me to develop this project. This is one of the most biggest project I have ever worked on and publish-able.",
+    engines: ["html5-plain", "css3-plain", "javascript-plain", "bootstrap-plain", "php-plain", "linux-plain", "amazonwebservices-original", "apache-plain", "nginx-original", "ssh-plain-wordmark"],
+    thumb: "smkjatim.png"
+  },
+  {
+    name: "Is Short!",
+    subtitle: "Another link shortener.",
+    description: "I was wondering if I actually could make a link shortener app, so I think about its algorithm in data exchange and... I made it! I have a lot things to do with it such as making a chrome extension for it, account system, etc.",
+    engines: ["html5-plain", "css3-plain", "javascript-plain", "nodejs-plain", "express-original", "mysql-plain"],
+    thumb: "isshort.png"
   }
 ];
