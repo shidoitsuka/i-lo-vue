68b4984d56a8ab11b18b3035234342cddc31ac5d
Brian <mail@standinshd.site>
2020-04-19 13:26:24 +0700

    all project thumbs are now in SVG format

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -5,41 +5,71 @@ export default [
     subtitle: "A just4fun discord bot.",
     description: "My first discord bot. I started this just for fun project when I was really active on discord.\nBack then, I don't really mind about its bug and stuffs, until I decided to take this project seriously because me & my friends actually had fun with it!",
     engines: ["javascript-plain", "nodejs-plain", "mysql-plain"],
-    thumb: "miku.png"
+    thumb: "miku-bot.svg",
+    github: "shidoitsuka/another-miku-bot",
+    link: "https://miku.standinshd.site/"
   },
   {
     name: "Miku Site",
     subtitle: "A just4fun discord bot.",
     description: "Miku seems to be actively used by my friends (or maybe others too!) So I decided to create a website exclusively for miku!",
     engines: ["html5-plain", "css3-plain", "javascript-plain", "jquery-plain", "nodejs-plain", "express-original"],
-    thumb: "miku-site.png"
+    thumb: "miku-site.svg",
+    github: "shidoitsuka/another-miku-bot",
+    link: "https://miku.standinshd.site/"
   },
   {
     name: "Pi RFID",
     subtitle: "Multi-purpose RFID scanner.",
     description: "My highschool, SMK Krian 1 trusted me to make an RFID system for their new canteen payment method. This card will also contains student's identity which means the student ID itself has a built in RFID system. It is also has a multi-level door lock system as well.",
     engines: ["html5-plain", "css3-plain", "javascript-plain", "nodejs-plain", "express-original", "mysql-plain", "cplusplus-plain", "python-plain"],
-    thumb: "rfid.png"
+    thumb: "pi-rfid.svg",
+    github: "shidoitsuka/pi-rfid",
+    link: ""
   },
   {
     name: "I Lo Vue",
     subtitle: "Yes, you read that right.",
     description: "This is what you're looking at, literally. I fell in love easily with stuffs and vue is one of it. So I decided to make my portofolio website!",
     engines: ["vuejs-plain", "sass-original", "javascript-plain", "babel-plain", "nodejs-plain"],
-    thumb: "i-lo-vue.png"
+    thumb: "i-lo-vue.svg",
+    github: "shidoitsuka/i-lo-vue",
+    link: ""
   },
   {
     name: "SMK Jatim",
     subtitle: "East Java's Vocational Highschool in one place.",
     description: "At late Q1 of 2020 East Java's Government trusted me to develop this project. This is one of the most biggest project I have ever worked on and publish-able.",
     engines: ["html5-plain", "css3-plain", "javascript-plain", "bootstrap-plain", "php-plain", "linux-plain", "amazonwebservices-original", "apache-plain", "nginx-original", "ssh-plain-wordmark"],
-    thumb: "smkjatim.png"
+    thumb: "smkjatim.svg",
+    github: "",
+    link: "https://smkjatim.id"
   },
   {
     name: "Is Short!",
     subtitle: "Another link shortener.",
     description: "I was wondering if I actually could make a link shortener app, so I think about its algorithm in data exchange and... I made it! I have a lot things to do with it such as making a chrome extension for it, account system, etc.",
     engines: ["html5-plain", "css3-plain", "javascript-plain", "nodejs-plain", "express-original", "mysql-plain"],
-    thumb: "isshort.png"
+    thumb: "isshort.svg",
+    github: "",
+    link: "https://isshort.tk"
+  },
+  {
+    name: "Adobe RPC",
+    subtitle: "Let your discord friends know you're using adobe apps.",
+    description: "I honestly created this project to kill my times. Yes. I was bored. And created this. This app basically detects your running apps and if there is adobe apps in it, it will be shown in your discord profile.",
+    engines: ["javascript-plain", "nodejs-plain"],
+    thumb: "adobe-rpc.svg",
+    github: "shidoitsuka/adobe-rpc",
+    link: ""
+  },
+  {
+    name: "Electron Kasir",
+    subtitle: "Multi-platform cashier app.",
+    description: "I created this project for my friend. He runs a store and it has a bad money management. So, I made this app and tell him to use it. But since it uses electron, it's heavy. Welp... Discontinued!",
+    engines: ["html5-plain", "css3-plain", "javascript-plain", "jquery-plain", "nodejs-plain", "atom-original", "mysql-plain"],
+    thumb: "electron-kasir.svg",
+    github: "shidoitsuka/electron-kasir",
+    link: ""
   }
 ];
