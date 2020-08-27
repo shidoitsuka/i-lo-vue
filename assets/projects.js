b9dcc11f86153f3858ffb6853e0283b405e86e28
Brian Harianja <shidoitsukaa@gmail.com>
2020-08-28 02:20:05 +0700

    added project file

---
+++ assets/projects.js
@@ -0,0 +1,65 @@
+export default [
+  {
+    name: "Miku bot",
+    overview: "A multi-purpose discord bot",
+    description: "At Q3 2015 I started using discord for my gaming purpose.",
+    icon: "",
+    github: "",
+    href: "",
+    lang: []
+  },
+  {
+    name: "Miku site",
+    overview: "",
+    description: "",
+    icon: "",
+    github: "",
+    href: "",
+    lang: []
+  },
+  {
+    name: "Isshort",
+    overview: "",
+    description: "",
+    icon: "",
+    github: "",
+    href: "",
+    lang: []
+  },
+  {
+    name: "smkjatim.id",
+    overview: "",
+    description: "",
+    icon: "",
+    github: "",
+    href: "",
+    lang: []
+  },
+  {
+    name: "Pi RFID",
+    overview: "",
+    description: "",
+    icon: "",
+    github: "",
+    href: "",
+    lang: []
+  },
+  {
+    name: "Smarthome system",
+    overview: "",
+    description: "",
+    icon: "",
+    github: "",
+    href: "",
+    lang: []
+  },
+  {
+    name: "Twibbon SKARISA",
+    overview: "",
+    description: "",
+    icon: "",
+    github: "",
+    href: "",
+    lang: []
+  }
+];
