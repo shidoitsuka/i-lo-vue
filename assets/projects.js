05583b760f8f16081069e24dc427cf0189d00b7b
Brian Harianja <shidoitsukaa@gmail.com>
2020-08-28 02:29:51 +0700

    added project thumbs

--- assets/projects.js
+++ assets/projects.js
@@ -3,28 +3,28 @@ export default [
     name: "Miku bot",
     overview: "A multi-purpose discord bot",
     description: "At Q3 2015 I started using discord for my gaming purpose.",
-    icon: "",
-    github: "",
-    href: "",
-    lang: []
+    icon: "miku-bot.svg",
+    github: "shidoitsuka/another-miku-bot.git",
+    href: false,
+    lang: [""]
   },
   {
     name: "Miku site",
-    overview: "",
-    description: "",
-    icon: "",
-    github: "",
-    href: "",
-    lang: []
+    overview: "Miku bot personal website",
+    description: "I created miku a website so it's easier for her to reach more users!",
+    icon: "miku-site.svg",
+    github: "shidoitsuka/another-miku-bot.git",
+    href: "https://miku.standinshd.site/",
+    lang: [""]
   },
   {
     name: "Isshort",
-    overview: "",
-    description: "",
-    icon: "",
-    github: "",
-    href: "",
-    lang: []
+    overview: "Another link shortener app",
+    description: "I tried to make a URL shortener & I kind of able to make it working",
+    icon: "isshort.svg",
+    github: false,
+    href: "https://isshort.tk/",
+    lang: [""]
   },
   {
     name: "smkjatim.id",
